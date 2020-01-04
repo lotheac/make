451d5da7b05ad766df6326ec532b8674722270ff
Lauri Tirkkonen <lotheac@iki.fi>
2020-01-04 19:41:44 +0200

    jobserver.h: fix copyright year

--- jobserver.h
+++ jobserver.h
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2019 Lauri Tirkkonen <lauri@hacktheplanet.fi>
+ * Copyright (c) 2020 Lauri Tirkkonen <lauri@hacktheplanet.fi>
  *
  * Permission to use, copy, modify, and distribute this software for any
  * purpose with or without fee is hereby granted, provided that the above
